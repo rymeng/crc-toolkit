f6676d2106d24661886de97498b0e4fb16b4de54
Rainer Meng <142570911+rymeng@users.noreply.github.com>
2025-05-21 14:17:59 +0800

    docs(project): add GPLv3 compliant file headers
    - Implemented standardized comment headers across all source files
    - Added SPDX-License-Identifier and copyright notices

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+@File    : __init__.py
+@Created : 2025/05/19
+@Author  : Rainer Meng
+@Desc    : Package identifier for CRC utilities
+@License : GPL-3.0 (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+Copyright (C) 2025  Rainer Meng
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
